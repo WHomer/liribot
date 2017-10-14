fdfd2a4cf1afff29166dd9fd1ed46c789f8f9462
William Homer <billyhomeriii@gmail.com>
2017-10-13 20:56:10 -0500

    master: working witht he movie omdb search

---
+++ server.js
@@ -0,0 +1,20 @@
+var request = require('request');
+
+function movieSearch(userInput) {
+	let movieTitle = userInput;
+
+	let url = 'http://www.omdbapi.com/?t=' + movieTitle + '&y=&plot=long&tomatoes=true&r=json&apikey=40e9cece';
+	
+	request(url, function(error, response, body) {
+		console.log("************  Movie  *************");
+		console.log("Title: " + JSON.parse(body)["Title"]);
+		console.log("Title Year: " + JSON.parse(body)["Year"]);
+		console.log("Title Plot: " + JSON.parse(body)["Plot"]);
+	});
+};
+
+// Arguments
+var userCommand = process.argv[2];
+var userInput = process.argv[3];
+
+movieSearch('Matrix');
